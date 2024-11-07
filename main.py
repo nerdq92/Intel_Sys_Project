4c054d2d2eaf234a06d97f50cde653eb0570f04c
nerdq92 <90452708+nerdq92@users.noreply.github.com>
2024-11-07 14:46:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -135,10 +135,8 @@ if st.session_state["genre_input"] is not None:
         st.link_button("Buy the Book", first_book_url)
         st.write("##### Do you like our recommendation?")        
         st.session_state["feedback"][f"{title}"] = st.feedback("stars")
+        st.write("##### Feedback History")        
         for title,feedback in st.session_state["feedback"].items():
             if feedback:
-                st.write(f"You gave the book {title} {feedback} stars.")        
-    # if st.button("Find another book"):
-    #     st.session_state["genre_input"] = None
-    #     st.rerun()
+                st.write(f"You gave the book :rainbow[{title}] {feedback} stars.") 
 
