2b7db98341292815c6c53277706eb4f17c3e0317
nerdq92 <90452708+nerdq92@users.noreply.github.com>
2024-11-07 11:48:12 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,53 +1,31 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+import pickle
 
-#INTEREST MODEL
+#PERSONALITY MODEL
 #load dataset
-interest_df = pd.read_csv("interest.csv")
+personality_df = pd.read_csv("personality_train.csv")
 
 #pre-processing
 gender_encoder = LabelEncoder()
-interest_df['Gender'] = gender_encoder.fit_transform(interest_df['Gender'])
-print(interest_df.head())
+personality_df['Gender'] = gender_encoder.fit_transform(personality_df['Gender'])
 
-X = interest_df.drop(["Interest"], axis=1)
-y = interest_df["Interest"]
+X = personality_df.drop(["Personality"], axis=1)
+y = personality_df["Personality"]
 
 # Train the model
-model = DecisionTreeClassifier()
+model = KNeighborsClassifier()
 model.fit(X, y)
+X_test = [1,30,2,2,1,7,8]
+new_data = pd.DataFrame([X_test], columns=["Gender", "Age","openness","neuroticism","conscientiousness","agreeableness","extraversion"])
+y_pred = model.predict(new_data)
+print(y_pred)
 
 #save model
-import pickle
-with open("interest_model.pkl", "wb") as f:
+with open("personality_model.pkl", "wb") as f:
     pickle.dump(model, f)
 with open('gender_encoder.pkl', 'wb') as f:
     pickle.dump(gender_encoder, f)
-
-#PRODUCT MODEL
-#load dataset
-product_df = pd.read_csv("product.csv")
-
-#pre-processing
-product_df['Gender'] = gender_encoder.fit_transform(product_df['Gender'])
-interest_encoder = LabelEncoder()
-product_df['Interest'] = interest_encoder.fit_transform(product_df['Interest'])
-product_encoder = LabelEncoder()
-product_df['Product_type'] = product_encoder.fit_transform(product_df['Product_type'])
-print(product_df.head())
-
-X = product_df.drop(["Product"], axis=1)
-y = product_df["Product"]
-
-# Train the model
-model = DecisionTreeClassifier()
-model.fit(X, y)
-
-#save model
-with open("product_model.pkl", "wb") as f:
-    pickle.dump(model, f)
-with open('interest_encoder.pkl', 'wb') as f:
-    pickle.dump(interest_encoder, f)
-with open('product_encoder.pkl', 'wb') as f:
-    pickle.dump(product_encoder, f)
